e8dab0e12893d68ffaa757cd67bbad3fb21ac320
Justin Ross <justin@thsjst.in>
2023-02-25 17:54:49 -0800

    Lots of tweaks. Fixed flutter animation

--- src/ts/scripts/ReactionSetupMenu.ts
+++ src/ts/scripts/ReactionSetupMenu.ts
@@ -1,178 +1,216 @@
-import { id as moduleId } from "../../../public/module.json"
-import { debouncedReload, saveAllReactionPNGs } from "./utils";
-import { resetDefaultReactions, ReactionOption } from "./settings";
+import { id as moduleId } from "../../../public/module.json";
+import {
+  calcAngleDegrees,
+  debouncedReload,
+  saveAllReactionPNGs,
+} from "./utils";
+import { loadReactionsPreset, ReactionOption } from "./settings";
 import { renderChatButtonBar } from "./hooks";
+import { reactionSets } from "./reactionsets";
 
 export class ReactionSetupMenu extends FormApplication {
-    // constructor(exampleOption) {
-    //     super(exampleOption);
+  // constructor(exampleOption) {
+  //     super(exampleOption);
+  // }
+
+  loadedJSON = {};
+  selectedPreset = "default";
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["form", "crowdgoeswild", "reactionSetup"],
+      popOut: true,
+      template: `modules/${moduleId}/templates/ReactionSetup.hbs`,
+      id: `${moduleId}-reaction-setup`,
+      title: "CrowdGoesWild - Reaction Setup",
+      width: 900,
+      submitOnChange: true,
+      closeOnSubmit: false,
+      resizable: true,
+    });
+  }
+
+  async getData() {
+    let data = {
+      currentReactions: [],
+      presets: reactionSets,
+      selectedPreset: this.selectedPreset,
+    };
+    data.currentReactions = game.settings.get(moduleId, "reactions") as [];
+    // let outputReactions = []
+    // for (const reaction of currentReactions) {
+    //     if(!reaction.id){
+
+    //     }
     // }
+    return data;
+  }
 
-    loadedJSON = {}
+  switchColors(inputEl1, inputEl2) {
+    let v1 = inputEl1.value;
+    let v2 = inputEl2.value;
+    console.log(inputEl1, inputEl2);
 
-    static get defaultOptions() {
-        return mergeObject(super.defaultOptions, {
-            classes: ['form', 'crowdgoeswild', 'reactionSetup'],
-            popOut: true,
-            template: `modules/${moduleId}/templates/ReactionSetup.hbs`,
-            id: `${moduleId}-reaction-setup`,
-            title: 'CrowdGoesWild - Reaction Setup',
-            width: 900,
-            submitOnChange: true,
-            closeOnSubmit: false,
-            resizable: true
-        });
+    inputEl1.value = v2;
+    inputEl2.value = v1;
+  }
+
+  async _updateObject(event, formData) {
+    const data = expandObject(formData);
+    let reactions = [];
+
+    for (const reaction of Object.values(data)) {
+      reactions[reaction.id] = reaction;
     }
+    await game.settings.set(moduleId, "reactions", reactions);
+    await this.render();
+    renderChatButtonBar();
+  }
 
-    async getData() {
-        let currentReactions = game.settings.get(moduleId, 'reactions') as []
-        // let outputReactions = []
-        // for (const reaction of currentReactions) {
-        //     if(!reaction.id){
+  activateListeners(html) {
+    super.activateListeners(html);
 
-        //     }
-        // }
-        return  currentReactions
-    }
+    html.find("#generateButton").on("click", async (ev) => {
+      this.close();
+      await saveAllReactionPNGs(true);
+      debouncedReload();
+    });
+    html.find("#resetButton").on("click", (ev) => {
+      this.showLoadPresetDialog();
+    });
+    html.find("#exportButton").on("click", (ev) => {
+      this.exportReactions();
+    });
+    html.find("#importButton").on("click", (ev) => {
+      this.showImportReactionsDialog();
+    });
+    html.find("#reactionPreset").on("change", (ev) => {
+      ev.stopPropagation();
+      this.selectedPreset = ev.currentTarget.value;
+    });
 
-    async _updateObject(event, formData) {
-        const data = expandObject(formData)
-        let reactions = []
+    html.find(".colorSwitch").on("click", (ev) => {
+      ev.stopPropagation();
+      let i1 = $(ev.target)
+        .parents(".reactionRow")
+        .find(".primaryColor input.color")
+        .first()
+        .get(0);
+      let i2 = $(ev.target)
+        .parents(".reactionRow")
+        .find(".secondaryColor input.color")
+        .first()
+        .get(0);
+      this.switchColors(i1, i2);
+    });
+  }
 
-        for (const reaction of Object.values(data)) {
-            reactions[reaction.id] = reaction
-        }
-        await game.settings.set(moduleId, 'reactions', reactions)
-        this.render()
-        renderChatButtonBar()
-    }
-
-    activateListeners(html) {
-        super.activateListeners(html)
-        html.find("#generateButton").on("click", async (ev)=>{
-            this.close()
-            await saveAllReactionPNGs(true)
-            debouncedReload()
-        })
-        html.find("#resetButton").on("click", (ev)=>{
-            this.showResetDefaultsDialog()
-        })
-        html.find("#exportButton").on("click", (ev)=>{
-            this.exportReactions()
-        })
-        html.find("#importButton").on("click", (ev)=>{
-            this.showImportReactionsDialog()
-        })
-    }
-
-    showImportReactionsDialog(){
-        let d = new Dialog({
-            title: "Import Reactions",
-            content: `
+  showImportReactionsDialog() {
+    let d = new Dialog({
+      title: "Import Reactions",
+      content: `
             <p>Import a set of reactions from a JSON file? All current reactions will be overwritten.</p>
             <input type="file" id="importer" name="reactionjson" class="cgw importer">
             `,
-            buttons: {
-             one: {
-              icon: '<i class="fas fa-check"></i>',
-              label: "Import",
-              callback: () => {
-                if(this.loadedJSON){
-                    this.saveReactionSetData(this.loadedJSON)
+      buttons: {
+        one: {
+          icon: '<i class="fas fa-check"></i>',
+          label: "Import",
+          callback: () => {
+            if (this.loadedJSON) {
+              this.saveReactionSetData(this.loadedJSON);
+            }
+          },
+        },
+        two: {
+          icon: '<i class="fas fa-times"></i>',
+          label: "Cancel",
+          callback: () => {},
+        },
+      },
+      default: "two",
+      render: (html) => {
+        $(html)
+          .find("#importer")
+          .on("change", (ev) => {
+            console.log("Loaded file");
+            let reader = new FileReader();
+            reader.onload = (readerEv) => {
+              try {
+                let loadedJSON = JSON.parse(readerEv.target.result);
+                if (this.validateLoadedJSON(loadedJSON)) {
+                  this.loadedJSON = loadedJSON;
                 }
+              } catch (error) {
+                console.log("Invalid JSON file");
+                this.loadedJSON = false;
               }
-             },
-             two: {
-              icon: '<i class="fas fa-times"></i>',
-              label: "Cancel",
-              callback: () => {}
-             }
-            },
-            default: "two",
-            render: html => {
-                $(html).find("#importer").on("change", (ev)=>{
-                    console.log("Loaded file");
-                    let reader = new FileReader()
-                    reader.onload = (readerEv)=>{
-                        try {
-                            let loadedJSON = JSON.parse(readerEv.target.result);
-                            if(this.validateLoadedJSON(loadedJSON)){
-                                this.loadedJSON = loadedJSON
-                            }
-                        } catch (error) {
-                            console.log("Invalid JSON file")
-                            this.loadedJSON = false
-                        }
-                        console.log(this.loadedJSON);
-                    }
-                    reader.readAsText(ev.target.files[0]);
-                })
-            },
-            close: html => {}
-           });
-        d.render(true);
+            };
+            reader.readAsText(ev.target.files[0]);
+          });
+      },
+      close: (html) => {},
+    });
+    d.render(true);
+  }
 
-    }
+  async exportReactions() {
+    let data = await game.settings.get(moduleId, "reactions");
+    let dataJSON = JSON.stringify(data);
+    saveDataToFile(dataJSON, "text/json", "reactions.json");
+  }
 
-    async exportReactions(){
-        let data = await game.settings.get(moduleId, 'reactions')
-        let dataJSON = JSON.stringify(data)
-        saveDataToFile(dataJSON, 'text/json', 'reactions.json')
-    }
+  async saveReactionSetData(data) {
+    // await game.settings.set(moduleId, 'reactions', data)
+  }
 
-    async saveReactionSetData(data){
-        // await game.settings.set(moduleId, 'reactions', data)
-    }
-
-    validateLoadedJSON(data){
-        let isValid = true
-        // make sure it's an array
-        if(Array.isArray(data)){
-            // make sure nobody's slipping in the wrong number of reactions
-            if(data.length == 6){
-                // check each row to make sure it has the right fields
-                for (const row of data) {
-                    for (const key in ReactionOption) {
-                        if(!(key in row)){
-                            isValid = false
-                            console.log(`Invalid JSON data in row ${row.id}: Missing ${key}`)
-                        }
-                    }
-                }
+  validateLoadedJSON(data) {
+    let isValid = true;
+    // make sure it's an array
+    if (Array.isArray(data)) {
+      // make sure nobody's slipping in the wrong number of reactions
+      if (data.length == 6) {
+        // check each row to make sure it has the right fields
+        for (const row of data) {
+          for (const key in ReactionOption) {
+            if (!(key in row)) {
+              isValid = false;
+              console.log(`Invalid JSON data in row ${row.id}: Missing ${key}`);
             }
-            else{
-                isValid = false
-            }
+          }
         }
-        return isValid
+      } else {
+        isValid = false;
+      }
     }
+    return isValid;
+  }
 
-    showResetDefaultsDialog(){
-        let d = new Dialog({
-            title: "Restore Defaults",
-            content: "<p>Reset the reaction set to defaults? All changes will be lost.</p>",
-            buttons: {
-             one: {
-              icon: '<i class="fas fa-check"></i>',
-              label: "Reset Defaults",
-              callback: () => resetDefaultReactions()
-             },
-             two: {
-              icon: '<i class="fas fa-times"></i>',
-              label: "Cancel",
-              callback: () => this.close()
-             }
-            },
-            default: "two",
-            render: html => {},
-            close: html => {}
-           });
-        d.render(true);
-    }
-
-
-
-
-    
+  showLoadPresetDialog() {
+    let d = new Dialog({
+      title: "Load Preset",
+      content: `<p>Load the ${
+        reactionSets[this.selectedPreset].label
+      } preset? Any changes you've made to reactions will be lost.</p>`,
+      buttons: {
+        one: {
+          icon: '<i class="fas fa-check"></i>',
+          label: "Load Preset",
+          callback: async () => {
+            await loadReactionsPreset(this.selectedPreset);
+            this.render(false);
+          },
+        },
+        two: {
+          icon: '<i class="fas fa-times"></i>',
+          label: "Cancel",
+          callback: () => this.close(),
+        },
+      },
+      default: "two",
+      render: (html) => {},
+      close: (html) => {},
+    });
+    d.render(true);
+  }
 }
-
--- src/ts/scripts/settings.ts
+++ src/ts/scripts/settings.ts
@@ -1,6 +1,7 @@
-import {ReactionSetupMenu} from "./ReactionSetupMenu";
-import { id as moduleId } from "../../../public/module.json"
+import { ReactionSetupMenu } from "./ReactionSetupMenu";
+import { id as moduleId } from "../../../public/module.json";
 import { debouncedReload, saveAllReactionPNGs } from "./utils";
+import { reactionSets } from "./reactionsets";
 
 export const defaultReactions: Array<ReactionOption> = [
   {
@@ -13,7 +14,7 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "",
     style: "fas",
     effect: "physics-floatUp",
-    directional: false
+    directional: false,
   },
   {
     id: 1,
@@ -25,7 +26,7 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "",
     style: "fas",
     effect: "physics-floatUp",
-    directional: false
+    directional: false,
   },
   {
     id: 2,
@@ -37,7 +38,7 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "#c1c1c1",
     style: "fa-duotone",
     effect: "physics-toss",
-    directional: true
+    directional: true,
   },
   {
     id: 3,
@@ -49,7 +50,7 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "#00a6ff",
     style: "fa-duotone",
     effect: "physics-drop",
-    directional: false
+    directional: false,
   },
   {
     id: 4,
@@ -61,7 +62,7 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "#eb8c34",
     style: "fa-duotone",
     effect: "physics-floatUp",
-    directional: false
+    directional: false,
   },
   {
     id: 5,
@@ -73,67 +74,67 @@ export const defaultReactions: Array<ReactionOption> = [
     secondaryColor: "rgba(255,255,255,0.6)",
     style: "fas",
     effect: "shutdown",
-    directional: false
-  }
-]
+    directional: false,
+  },
+];
 
 export const ReactionOption = {
-    id: 0,
-    title: "",
-    icon: "",
-    primaryColor: "",
-    secondaryColor: "",
-    style: "",
-    speed: 0,
-    effect: "",
-    directional: false,
-    enabled: true
-}
+  id: 0,
+  title: "",
+  icon: "",
+  primaryColor: "",
+  secondaryColor: "",
+  style: "",
+  speed: 0,
+  effect: "",
+  directional: false,
+  enabled: true,
+};
 
-export type ReactionOption = typeof ReactionOption
+export type ReactionOption = typeof ReactionOption;
 
-export async function resetDefaultReactions(){
-    console.log(defaultReactions);
-    await game.settings.set(moduleId, 'reactions', defaultReactions)
-    await saveAllReactionPNGs(true)
-    debouncedReload()
-    return 
+export async function loadReactionsPreset(presetName: string) {
+  let reactions = reactionSets[presetName].reactions;
+  await game.settings.set(moduleId, "reactions", reactions);
+  //   await saveAllReactionPNGs(true);
+  //   debouncedReload();
+  return;
 }
 
 export function registerSettings() {
-    console.log("Registering CGW Settings")
-    game.settings.register(moduleId, 'reactions', {
-        name: 'Reaction',
-        hint: 'The list of reactions usable by your players',
-        scope: 'world', // "world" = sync to db, "client" = local storage
-        config: false, // false if you dont want it to show in module config
-        type: Array, // Number, Boolean, String, Object
-        default: defaultReactions,
-        onChange: value => { // value is the new value of the setting
-        }
-    });
+  console.log("Registering CGW Settings");
+  game.settings.register(moduleId, "reactions", {
+    name: "Reaction",
+    hint: "The list of reactions usable by your players",
+    scope: "world", // "world" = sync to db, "client" = local storage
+    config: false, // false if you dont want it to show in module config
+    type: Array, // Number, Boolean, String, Object
+    default: defaultReactions,
+    onChange: (value) => {
+      // value is the new value of the setting
+    },
+  });
 
-    game.settings.register(moduleId, 'maxdisplayed', {
-        name: 'Maximum Simultaneous Reactions',
-        hint: `Turn this down if you're running into performance issues from players spamming reactions.`,
-        scope: 'client',
-        config: false,
-        type: Number,
-        range: {
-            min: 10,
-            max: 200,
-            step: 5
-        },
-        default: 200
-    })
+  game.settings.register(moduleId, "maxdisplayed", {
+    name: "Maximum Simultaneous Reactions",
+    hint: `Turn this down if you're running into performance issues from players spamming reactions.`,
+    scope: "client",
+    config: false,
+    type: Number,
+    range: {
+      min: 10,
+      max: 200,
+      step: 5,
+    },
+    default: 200,
+  });
 
-    game.settings.registerMenu(moduleId, 'reactionSetup', {
-        name: "Reactions",
-        label: "Configure Available Reactions", // The text label used in the button
-        hint: "Use this menu to create up to eight reactions for your players to use during play.",
-        icon: "fas fa-bars", // A Font Awesome icon used in the submenu button
-        type: ReactionSetupMenu, // A FormApplication subclass
-        restricted: true // Restrict this submenu to gamemaster only?
-    });
+  game.settings.registerMenu(moduleId, "reactionSetup", {
+    name: "Reactions",
+    label: "Configure Available Reactions", // The text label used in the button
+    hint: "Use this menu to create up to eight reactions for your players to use during play.",
+    icon: "fas fa-bars", // A Font Awesome icon used in the submenu button
+    type: ReactionSetupMenu, // A FormApplication subclass
+    restricted: true, // Restrict this submenu to gamemaster only?
+  });
 }
-
